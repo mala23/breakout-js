b43088e362860230ea3ecfc48de0a249ba3dc658
mala23 <mail@mala23.me>
2017-09-01 00:32:07 +0200

    add circle

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d")
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
+ctx.beginPath()
+ctx.rect(20, 40, 50, 50)
 ctx.fillStyle = "#FF0000"
+ctx.fill()
+ctx.closePath()
+
+ctx.beginPath()
+ctx.arc(240, 160, 20, 0, Math.PI*2, false)
+ctx.fillStyle = "green"
 ctx.fill()
 ctx.closePath()
 
