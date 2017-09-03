f6c1d8fb1002052e0158f31ea92055662331e9ce
mala23 <mail@mala23.me>
2017-09-03 17:05:00 +0200

    create paddle and key event listeners

--- script.js
+++ script.js
@@ -9,10 +9,27 @@ var y = canvas.height-30
 var dx = 2
 var dy = -2
 
+var ballRadius = 10
+
+var paddleHeight = 10
+var paddleWidth = 75
+var paddleX = (canvas.width-paddleWidth)/2
+
+var rightPressed = false
+var leftPressed = false
+
 function drawBall() {
   ctx.beginPath()
-  ctx.arc(x, y, 10, 0, Math.PI*2)
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2)
   ctx.fillStyle = "#0095DD";
+  ctx.fill()
+  ctx.closePath()
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight)
+  ctx.fillStyle = "#0095DD"
   ctx.fill()
   ctx.closePath()
 }
@@ -20,8 +37,45 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawBall()
+
+  if(x + dx > canvas.width || x + dx < 0) {
+    dx = -dx
+  }
+
+  if(y + dy > canvas.height || y + dy < 0) {
+    dy = -dy
+  }
+
   x += dx
   y += xy
+
+  if(rightPressed) {
+    paddleX += 7
+  }
+  if(leftPressed) {
+    paddleY -= 7
+  }
+}
+
+document.addEventListener("keydown", keyDownHandler, false)
+document.addEventListener("keyup", keyUpHandler, false)
+
+function keyDownHandler(e) {
+  if(e.keyCode == 39) {
+    rightPressed = true
+  }
+  else if(e.keyCode == 37) {
+    leftPressed = true
+  }
+}
+
+function keyUpHandler(e) {
+  if(e.keyCode == 39) {
+    rightPressed = false
+  }
+  else if(e.keyCode == 37) {
+    leftPressed = false
+  }
 }
 
 setInterval(draw, 10)
