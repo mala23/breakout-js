b879636547c9db67e1f58d3698d6cd5ef5bf28b0
mala23 <mail@mala23.me>
2017-09-06 21:25:35 +0200

    implement mouse control

--- script.js
+++ script.js
@@ -108,6 +108,7 @@ function draw() {
 
 document.addEventListener("keydown", keyDownHandler, false)
 document.addEventListener("keyup", keyUpHandler, false)
+document.addEventListener("mousemove", mouseMoveHandler, false)
 
 function keyDownHandler(e) {
   if(e.keyCode == 39) {
@@ -124,6 +125,13 @@ function keyUpHandler(e) {
   }
   else if(e.keyCode == 37) {
     leftPressed = false
+  }
+}
+
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft
+  if(relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2
   }
 }
 
