9dca2b69f9679d39b90dbfc5d522d22ab84373c4
mala23 <mail@mala23.me>
2017-09-06 20:30:00 +0200

    draw blocks in correct location

--- script.js
+++ script.js
@@ -25,13 +25,11 @@ var brickHeight = 20
 var brickPadding = 10
 var brickOffsetTop = 30
 var brickOffsetLeft = 30
-var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft
-var brickY = (r*(brickHeight+brickPadding))+brickOffsettTop
 
 var bricks = []
-for(c=0 c<brickColumnCount c++) {
+for(c=0; c<brickColumnCount; c++) {
   bricks[c] = []
-  for(r=0 r<brickRowCount r++) {
+  for(r=0; r<brickRowCount; r++) {
     bricks[c][r] = { x: 0, y: 0 }
   }
 }
@@ -54,12 +52,14 @@ function drawPaddle() {
 }
 
 function drawBricks() {
-  for(c=0 c<brickColumnCount c++) {
-    for(r=0 r<brickRowCount r++) {
-      bricks[c][r].x = 0
-      bricks[c][r].y = 0
+  for(c=0; c<brickColumnCount; c++) {
+    for(r=0; r<brickRowCount; r++) {
+      var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft
+      var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop
+      bricks[c][r].x = brickX
+      bricks[c][r].y = brickY
       ctx.beginPath()
-      ctx.rect(0, 0, brickWidth, brickHeight)
+      ctx.rect(brickX, brickY, brickWidth, brickHeight)
       ctx.fillStyle = "#0095DD"
       ctx.fill()
       ctx.closePath()
